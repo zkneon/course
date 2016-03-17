48ea9f59aaec0f5d30510ac6c34f52445b1c621f
zkh <zk.neon@gmail.com>
2016-03-17 22:04:04 +0300

    add hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+def app (environ, start_response):
+  status = '200 OK'
+  response_headers = [('Content-type','text/plain')]
+  start_response(status, response_headers)
+  resp = environ['QUERY_STRING'].split("&")
+  resp = [item+"\r\n" for item in resp]
+  return resp
