1777447242d0237559606100a15ff03c12611a36
zkneon <zk.neon@gmail.com>
2016-03-17 10:56:50 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
