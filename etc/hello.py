c0d702d0dfdc5bbf3e1aa914755957d006f9fa0d
zkh <zk.neon@gmail.com>
2016-03-21 22:16:26 +0300

    add gunicorn conf

--- etc/hello.py
+++ etc/hello.py
@@ -1,1 +1,11 @@
-conf
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web/',
+    'python': '/usr/bin/python',                                              
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=16',
+        '--timeout=60',
+        'hello.py',
+    ),
+}
