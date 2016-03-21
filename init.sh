c78be73f409c0da0b6903fce69bc3f09fe610959
zkh <zk.neon@gmail.com>
2016-03-21 22:26:29 +0300

    gunicorn add to init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
